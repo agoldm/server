1ca3471d6ae61b15f77aca31c7c6c5b580a43ebc
shiraDoron <shira27042000@gmail.com>
2022-07-27 21:29:55 +0300

    course

--- db/models/courseModel.js
+++ db/models/courseModel.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
 
-const times = new mongoose.Schema({
-    time_in_day: { type: Date },
-    day_in_week: { type: Date },
-    start_date: { type: Date },
-    end_date: { type: Date },
-    long: { type: String },
-});
+// const times = new mongoose.Schema({
+//     time_in_day: { type: Date },
+//     day_in_week: { type: Date },
+//     start_date: { type: Date },
+//     end_date: { type: Date },
+//     long: { type: String },
+// });
 
 const courseSchema = new mongoose.Schema({
     name: { type: String, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
-    course_times: { type: times, default: {} },
+    //course_times: { type: times, default: {} },
     price_per_time: { type: String, required: true },
     max_student: { type: Number, required: true },
     image: { type: String },
     teacher_id: { type: String },
     num_of_classes: { type: Number },
+    start_date: { type: Date },
+    end_date: { type: Date },
+    long: { type: String },
     students_ids: { type: [mongoose.Schema.Types.ObjectId] },
     rating: { type: Number },
     status: { type: Boolean, default: true }
